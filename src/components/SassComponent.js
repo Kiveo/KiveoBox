88470ff5048edea5f128e3b3f9e8529301a89774
Mav <30327928+Kiveo@users.noreply.github.com>
2019-06-27 11:13:58 -0400

    Change to sass testbox
    initial change of structure

---
+++ src/components/SassComponent.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const SassComponent = props => {
+  /* -- STATE -- */
+  const [state, setState] = useState();
+  /* -- LIFECYCLE -- */
+  /* -- HANDLERS -- */
+  /* -- RENDER -- */
+  return (
+    <div>
+      <h1>Sass Component Heading</h1>
+      <p>Paragraph</p>
+      <p>
+        A second Paragraph
+        <span>Span inside p2</span>
+      </p>
+    </div>
+  );
+};
+
+SassComponent.propTypes = {
+  // add propTypes here
+};
+
+export default SassComponent;
